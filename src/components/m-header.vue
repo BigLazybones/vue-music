a54a215e3068aa9fe8b1ca062791f6409b409fbb
BigLazybones <125787989@qq.com>
2018-12-12 18:45:13 +0800

    Initial commit
    页面骨架开发
    包括页面入口、header 组件的编写、路由配置及顶导 tab 组件开发

---
+++ src/components/m-header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="text">Chicken Music</h1>
+        <router-link tag="div" class="mine" to="/user">
+            <i class="icon-mine"></i>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Recommend from '../components/recommend'
+import Singer from '../components/singer'
+import Rank from '../components/rank'
+import Search from '../components/search'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
+    }
+  ]
+})
