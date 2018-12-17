bacf9b0c8800ff44419e8ff90b1ebc46b43d7a40
BigLazybones <125787989@qq.com>
2018-12-17 23:28:39 +0800

    第四章
    4-1 页面简介+轮播图数据分析
    4-2 jsonp原理介绍+Promise封装
    4-3 jsonp的应用+轮播图数据抓取

--- src/components/recommend.vue
+++ src/components/recommend.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        推荐页面
+  <div class="recommend" ref="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper"></div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
     </div>
+  </div>
 </template>
+
+<script type="text/ecmascript-6">
+import { getRecommend } from '../api/recommend'
+import { ERR_OK } from '../api/config'
+
+export default {
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend: function() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
+}
+</script>
